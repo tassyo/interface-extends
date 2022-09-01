643e63fd1e6d9386dc95619ccfa54d198f888d80
Tassyo Monteiro <tassyo.monteiro@hotmail.com>
2022-08-31 23:59:55 -0300

    update Client

--- Client.js
+++ Client.js
@@ -1,14 +1,10 @@
 export class Client {
-    name
-
-    _cpf
-
-    get cpf() {
-        return this._cpf
-    }
-
     constructor(name, cpf) {
         this.name = name
         this._cpf = cpf
+    }
+
+    get cpf() {
+        return this._cpf
     }
 }
